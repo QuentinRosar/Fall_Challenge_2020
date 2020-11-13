633bcb64eb09734d675f6929532709883f817051
Quentin Rosar <rosarquentin@gmail.com>
2020-11-13 14:16:21 +0100

    Refactor : add Methods makeAction

---
+++ src/main/java/party/MakeAction.java
@@ -0,0 +1,42 @@
+package party;
+
+import models.Action;
+import models.Inventory;
+
+import java.util.List;
+
+public class MakeAction {
+
+    public static String toMakeCommandBestPrice(List<Action> actions, Inventory inventory) {
+        actions.sort(Action::compareTo);
+
+        for(Action action : actions) {
+            if(enoughIndredientsInInventory(inventory,action)) {
+                return  String.valueOf(action.getId());
+            }
+        }
+        return String.valueOf(0);
+    }
+
+    public static boolean enoughIndredientsInInventory(Inventory inventory, Action action) {
+        boolean ingredient0IsOk = inventory.getInv0() >= action.getDelta0();
+        boolean ingredient1IsOk = inventory.getInv1() >= action.getDelta1();
+        boolean ingredient2IsOk = inventory.getInv2() >= action.getDelta2();
+        boolean ingredient3IsOK = inventory.getInv3() >= action.getDelta3();
+
+        return ingredient0IsOk && ingredient1IsOk && ingredient2IsOk && ingredient3IsOK;
+    }
+
+    public static int removeActionUse(List<Action> actions, String idAction) {
+        int index = 0;
+
+        for(Action action : actions) {
+            int id = Integer.valueOf(idAction);
+            if(action.getId() == id) {
+                return index;
+            }
+            index++;
+        }
+        return index;
+    }
+}
--- src/main/java/party/Player.java
+++ src/main/java/party/Player.java
@@ -58,46 +58,15 @@ public class Player {
 
 
             // in the first league: BREW <id> | WAIT; later: BREW <id> | CAST <id> [<times>] | LEARN <id> | REST | WAIT
-            final String idAction = toMakeCommandBestPrice(actions, inventories.get(0));
-            System.err.println(actions.remove(removeActionUse(actions, idAction)));
+            final String idAction = MakeAction.toMakeCommandBestPrice(actions, inventories.get(0));
+            System.err.println(actions.remove(MakeAction.removeActionUse(actions, idAction)));
 
 
             System.out.println("BREW " + idAction);
         }
     }
 
-    public static String toMakeCommandBestPrice(List<Action> actions, Inventory inventory) {
-        actions.sort(Action::compareTo);
 
-        for(Action action : actions) {
-            if(enoughIndredientsInInventory(inventory,action)) {
-                return  String.valueOf(action.getId());
-            }
-        }
-        return String.valueOf(0);
-    }
-
-    public static boolean enoughIndredientsInInventory(Inventory inventory, Action action) {
-       boolean ingredient0IsOk = inventory.getInv0() >= action.getDelta0();
-       boolean ingredient1IsOk = inventory.getInv1() >= action.getDelta1();
-       boolean ingredient2IsOk = inventory.getInv2() >= action.getDelta2();
-       boolean ingredient3IsOK = inventory.getInv3() >= action.getDelta3();
-
-        return ingredient0IsOk && ingredient1IsOk && ingredient2IsOk && ingredient3IsOK;
-    }
-
-    public static int removeActionUse(List<Action> actions, String idAction) {
-        int index = 0;
-
-        for(Action action : actions) {
-            int id = Integer.valueOf(idAction);
-            if(action.getId() == id) {
-                return index;
-            }
-            index++;
-        }
-        return index;
-    }
 
 }
 
