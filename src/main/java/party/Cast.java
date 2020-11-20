5165f193ae3c36fbf863b137eec5577c9d8b62f0
Quentin Rosar <rosarquentin@gmail.com>
2020-11-20 15:17:31 +0100

    Refactor(Cast): order to brew

--- src/main/java/party/Cast.java
+++ src/main/java/party/Cast.java
@@ -11,10 +11,10 @@ import java.util.stream.Collectors;
 
 public class Cast {
 
-    public static String operationForMakeOrder(List<Action> orders, List<Action> casts, Inventory inventory) {
-        if(enoughItemsForOrder(inventory, orders)) {
-            return makeOrder(orders, inventory);
-        } else if(castableAndEnoughitemsForCast(casts, inventory) && sizeInventory(inventory) <= 10) {
+    public static String operationForMakeOrder(List<Action> brews, List<Action> casts, Inventory inventory) {
+        if(enoughItemsForBrew(inventory, brews)) {
+            return makeBrew(brews, inventory);
+        } else if(castableAndEnoughItemsForCast(casts, inventory) && sizeInventoryForCast(inventory) <= 10) {
             List<Action> spellCastable = casts.stream().filter(cast -> cast.getSpell().isCastable()).collect(Collectors.toList());
             Action bestAction = new Action();
 
@@ -30,7 +30,7 @@ public class Cast {
             } else {
                 return "REST";
             }
-        } else if(sizeInventory(inventory) > 8) {
+        } else if(sizeInventoryForCast(inventory) > 8) {
             List<Action> spellBase = makeSpellsBase(casts);
             Action bestAction = new Action();
 
@@ -49,9 +49,10 @@ public class Cast {
         }
     }
 
-    private static boolean enoughItemsForOrder(Inventory inventory, List<Action> orders) {
+    private static boolean enoughItemsForBrew(Inventory inventory, List<Action> brews) {
         boolean result = false;
-        for(Action order : orders) {
+
+        for(Action order : brews) {
             if(inventory.getInv0() + order.getItems().getDelta0() >= 0
                     && inventory.getInv1() + order.getItems().getDelta1() >= 0
                     && inventory.getInv2() + order.getItems().getDelta2() >= 0
@@ -59,29 +60,34 @@ public class Cast {
                 result = true;
             }
         }
+
         return result;
     }
 
-    private static String makeOrder(List<Action> actions, Inventory inventory) {
+    private static String makeBrew(List<Action> brews, Inventory inventory) {
         Action result = new Action();
-        for(Action order : actions) {
-            if(inventory.getInv0() + order.getItems().getDelta0() >= 0
-                    && inventory.getInv1() + order.getItems().getDelta1() >= 0
-                    && inventory.getInv2() + order.getItems().getDelta2() >= 0
-                    && inventory.getInv3() + order.getItems().getDelta3() >= 0) {
-                result = order;
+
+        for(Action brew : brews) {
+            if(inventory.getInv0() + brew.getItems().getDelta0() >= 0
+                    && inventory.getInv1() + brew.getItems().getDelta1() >= 0
+                    && inventory.getInv2() + brew.getItems().getDelta2() >= 0
+                    && inventory.getInv3() + brew.getItems().getDelta3() >= 0) {
+                result = brew;
             }
         }
+
         return "BREW " + String.valueOf(result.getId());
     }
 
-    private static boolean castableAndEnoughitemsForCast(List<Action> casts, Inventory inventory) {
+    private static boolean castableAndEnoughItemsForCast(List<Action> casts, Inventory inventory) {
         boolean result = false;
+
         for(Action cast : casts) {
             if(cast.getSpell().isCastable() && enoughItemForCast(inventory, cast)) {
                 result = true;
             }
         }
+
         return  result;
     }
 
@@ -93,19 +99,17 @@ public class Cast {
     }
 
     private static boolean enoughPlaceInventory(Inventory inventory, Action cast) {
-        int sumInventory = sizeInventory(inventory);
+        int sumInventory = sizeInventoryForCast(inventory);
         int sumCast = cast.getItems().getDelta0() + cast.getItems().getDelta1() + cast.getItems().getDelta2() + cast.getItems().getDelta3();
 
         return (sumInventory + sumCast) < 10;
     }
 
-    private static int sizeInventory(Inventory inventory) {
+    private static int sizeInventoryForCast(Inventory inventory) {
         return inventory.getInv0() + inventory.getInv1() + inventory.getInv2() + inventory.getInv3();
     }
 
     private static boolean bestAction(Items itemsMiss, Action cast, Action bestAction) {
-        // retourn si cast.item0 > bestAction.item0 et que order item0 < 0....
-
         boolean item0 = cast.getItems().getDelta0() > bestAction.getItems().getDelta0();
         boolean item1 = cast.getItems().getDelta1() > bestAction.getItems().getDelta1();
         boolean item2 = cast.getItems().getDelta2() > bestAction.getItems().getDelta2();
@@ -114,17 +118,20 @@ public class Cast {
         return item0 || item1 || item2 || item3;
     }
 
-    private static Items makeItemsMiss(Action order, Inventory inventory) {
+    private static Items makeItemsMiss(Action brew, Inventory inventory) {
         Items itemsMiss = new Items();
-        itemsMiss.setDelta0(order.getItems().getDelta0() + inventory.getInv0());
-        itemsMiss.setDelta1(order.getItems().getDelta1() + inventory.getInv1());
-        itemsMiss.setDelta2(order.getItems().getDelta2() + inventory.getInv2());
-        itemsMiss.setDelta3(order.getItems().getDelta3() + inventory.getInv3());
+
+        itemsMiss.setDelta0(brew.getItems().getDelta0() + inventory.getInv0());
+        itemsMiss.setDelta1(brew.getItems().getDelta1() + inventory.getInv1());
+        itemsMiss.setDelta2(brew.getItems().getDelta2() + inventory.getInv2());
+        itemsMiss.setDelta3(brew.getItems().getDelta3() + inventory.getInv3());
+
         return itemsMiss;
     }
 
     private static List<Action> makeSpellsBase(List<Action> casts) {
         List<Action> spellBase = new ArrayList<>();
+
         for(Action cast : casts) {
             if(cast.getId() == 78
                     || cast.getId() == 79
@@ -133,6 +140,7 @@ public class Cast {
                 spellBase.add(cast);
             }
         }
+
         return spellBase;
     }
 }
