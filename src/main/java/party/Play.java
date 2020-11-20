e674eb72266e04cfd3064d09d99889c007dcb798
Quentin Rosar <rosarquentin@gmail.com>
2020-11-20 15:07:56 +0100

    Refactor(Play) : delete data orderToMake don't use

--- src/main/java/party/Play.java
+++ src/main/java/party/Play.java
@@ -5,6 +5,7 @@ import models.Inventory;
 import models.Items;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,14 +14,7 @@ public class Play {
     public static String play(List<Action> actionList, Inventory inventory) {
         List<Action> brews = actionList.stream().filter(f -> f.getActionType().equals("BREW")).collect(Collectors.toList());
         List<Action> casts = actionList.stream().filter(f -> f.getActionType().equals("CAST")).collect(Collectors.toList());
-        List<Action> learns = actionList.stream().filter(f -> f.getActionType().equals("LEARN")).collect(Collectors.toList());
 
-        Action orderToMake = MakeOrder.takeBestOrderToMake(actionList.stream().filter(f -> f.getActionType().equals("BREW")).collect(Collectors.toList()));
-
-        return "WAIT";
+       return Cast.operationForMakeOrder(brews, casts, inventory);
     }
-
-/*    private boolean enoughSpaceForCast(Inventory inventory, Action cast) {
-
-    }*/
 }
